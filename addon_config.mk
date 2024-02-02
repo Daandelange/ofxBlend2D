2f726db6fe828a0e7269e8673c142b29b66d4baf
Daan <idaany@gmail.com>
2024-02-02 22:30:42 +0100

    Fix addon config for makefile builds

--- addon_config.mk
+++ addon_config.mk
@@ -7,6 +7,8 @@ meta:
 
 common:
 	ADDON_DEPENDENCIES = "ofxFps"
+
+	# Manually add includes
 	ADDON_INCLUDES = src/
 	ADDON_INCLUDES += libs/asmjit/include
 	ADDON_INCLUDES += libs/blend2d/include
@@ -15,14 +17,21 @@ common:
 	ADDON_INCLUDES += libs/asmjit/src
 	ADDON_INCLUDES += libs/blend2d/src
 
-	ADDON_SOURCES_EXCLUDE += libs/asmjit/test
-	ADDON_SOURCES_EXCLUDE += libs/blend2d/test
-	ADDON_SOURCES_EXCLUDE += libs/blend2d/build
+	# Force-exclude these includes
+	ADDON_INCLUDES_EXCLUDE = libs/asmjit/test/%
+	ADDON_INCLUDES_EXCLUDE += libs/blend2d/test/%
+	ADDON_INCLUDES_EXCLUDE += libs/blend2d/build/%
+
+	# Note: ADDON_SOURCES are automatically parsed
+	# Force-exclude these sources
+	ADDON_SOURCES_EXCLUDE = libs/asmjit/test/%
+	ADDON_SOURCES_EXCLUDE += libs/blend2d/test/%
+	ADDON_SOURCES_EXCLUDE += libs/blend2d/build/%
 
 	# See : https://blend2d.com/doc/build-instructions.html#BuildingWithoutCMake
-	# ADDON_DEFINES seems to be ignored by make ... using CFLAGS too
 	ADDON_DEFINES = NDEBUG
 	ADDON_DEFINES += BL_BUILD_OPT_SSE2
+	# ADDON_DEFINES seems to be ignored by make ... using CFLAGS too
 	ADDON_CPPFLAGS = -DNDEBUG -DBL_BUILD_OPT_SSE2
 
 osx:
